9e00a2bc7eca86760db8454fd04400ae72b489a1
sai-myne <ahlusir@gmail.com>
2020-08-10 13:18:16 +0800

    Repeating Animations

--- pizzajoint/src/components/Home.js
+++ pizzajoint/src/components/Home.js
@@ -4,9 +4,13 @@ import { motion } from 'framer-motion';
 
 const buttonVariants = {
   hover: {
-    scale: [1, 1.1, 1, 1.1, 1, 1.1,1],
+    scale: 1.1,
     textShadow: "0px 0px 8px rgb(255,255,255)", 
-    boxShadow: "0px 0px 8px rgb(255,255,255)"     
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: {
+      duration: 0.3,
+      yoyo: Infinity
+    }     
   }
 }
 
@@ -14,7 +18,7 @@ const Home = () => {
   return (
     <motion.div className="home container"
       initial={{ opacity: 0 }}
-      animate={{ opacity: 1}}
+      animate={{ opacity: 1 }}
       transition={{ delay: 1.5, duration: 1.5 }}
     >
       <h2>
